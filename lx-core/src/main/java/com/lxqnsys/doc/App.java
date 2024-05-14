aeca05265d8a089fed908850b3622b9256ceb6e0
wuzhenhong <wuzhenhong@banksteel.com>
2024-05-14 08:47:21 +0800

    feat：修改配置

--- lx-core/src/main/java/com/lxqnsys/doc/App.java
+++ lx-core/src/main/java/com/lxqnsys/doc/App.java
@@ -4,11 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(exclude = RedisAutoConfiguration.class)
+@SpringBootApplication
 @EnableScheduling
 @EnableTransactionManagement(proxyTargetClass = true)
 @MapperScan(basePackages = {"com.lxqnsys.doc.dao.mapper"})
--- lx-core/src/main/java/com/lxqnsys/doc/config/RedisConfig.java
+++ lx-core/src/main/java/com/lxqnsys/doc/config/RedisConfig.java
@@ -1,9 +1,6 @@
 package com.lxqnsys.doc.config;
 
-import com.lxqnsys.doc.properties.RedissonProperty;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
@@ -12,65 +9,40 @@ import org.redisson.config.SentinelServersConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
-import redis.clients.jedis.JedisPoolConfig;
-import redis.clients.jedis.JedisSentinelPool;
 
 /**
  * @author wuzhenhong
  * @date 2023/8/9 15:55
  */
 @Configuration
-@EnableConfigurationProperties(RedissonProperty.class)
 @ConditionalOnClass({RedissonClient.class})
 @ConditionalOnProperty("public.redis.sentinel.node")
 public class RedisConfig {
 
 
     @Autowired
-    private RedissonProperty redissonProperty;
+    private RedisProperties redisProperties;
 
     @Bean
-    public JedisPoolConfig jedisPoolConfig() {
-        JedisPoolConfig config = new JedisPoolConfig();
-        config.setTestWhileIdle(true);
-        config.setMinEvictableIdleTimeMillis(60000);
-        config.setTimeBetweenEvictionRunsMillis(30000);
-        config.setNumTestsPerEvictionRun(-1);
-        config.setMaxTotal(500);
-        config.setMaxIdle(20);
-        config.setMinIdle(8);
-        return config;
-    }
-
-    @Bean
-    public JedisSentinelPool jedisSentinelPool(JedisPoolConfig jedisPoolConfig) {
-        HashSet<String> infos = new HashSet<>();
-        String[] split = redissonProperty.getNode().split(",");
-        infos.addAll(Arrays.asList(split));
-        return new JedisSentinelPool(redissonProperty.getMaster(), infos, jedisPoolConfig, 5000,
-            redissonProperty.getPassword());
-    }
-
-    @Bean
-    public RedissonClient redissonClient(RedissonProperty properties) {
+    public RedissonClient redissonClient(RedisProperties properties) {
         Config config = new Config();
-        String[] nodes = properties.getNode().split(",");
-        List<String> newNodes = new ArrayList(nodes.length);
+        List<String> nodes = properties.getSentinel().getNodes();
+        List<String> newNodes = new ArrayList(nodes.size());
 
         for (String node : nodes) {
             newNodes.add(node.startsWith("redis://") ? node : "redis://" + node);
         }
 
         SentinelServersConfig serverConfig = config.useSentinelServers()
-            .addSentinelAddress(newNodes.toArray(new String[nodes.length]))
-            .setMasterName(properties.getMaster())
-            .setTimeout(properties.getTimeout())
-            .setMasterConnectionPoolSize(properties.getMasterSize())
-            .setSlaveConnectionPoolSize(properties.getSlaveSize());
+            .addSentinelAddress(newNodes.toArray(new String[nodes.size()]))
+            .setMasterName(properties.getSentinel().getMaster())
+            .setTimeout(5000)
+            .setMasterConnectionPoolSize(10)
+            .setSlaveConnectionPoolSize(10);
 
         if (!StringUtils.isEmpty(properties.getPassword())) {
             serverConfig.setPassword(properties.getPassword());
