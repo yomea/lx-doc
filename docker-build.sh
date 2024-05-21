c4d57462a44232295652f9b942dab6d7f84b38e6
wuzhenhong <wuzhenhong@banksteel.com>
2024-05-21 16:09:05 +0800

    feat：docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,8 @@
 
 mvn -DskipTests -U clean package
 
+mkdir -p /usr/logs/
+
 docker build -t lx-doc:1.0 .
 
 docker run -dit -p 9222:9222 -v /usr/logs/:/usr/logs/ -e ARGS='--log.home=/usr/logs --loglevel=INFO' --name lx-doc lx-doc:1.0
