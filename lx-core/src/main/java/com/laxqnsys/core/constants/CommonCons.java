9421c1813957faf7d3871c5c479f362c4e769c31
wuzhenhong <wuzhenhong@banksteel.com>
2024-06-24 16:17:11 +0800

    feat：延长登录过期时间

--- lx-core/src/main/java/com/laxqnsys/core/constants/CommonCons.java
+++ lx-core/src/main/java/com/laxqnsys/core/constants/CommonCons.java
@@ -1,7 +1,5 @@
 package com.laxqnsys.core.constants;
 
-import java.time.format.DateTimeFormatter;
-
 /**
  * @author wuzhenhong
  * @date 2024/5/14 9:11
@@ -12,11 +10,8 @@ public class CommonCons {
 
     public static final String AES_KEY = "23423jAFsdf323ls";
 
-    public static final long LOGIN_EXPIRE_SECONDS = 30 * 60 * 1000;
-    public static final long LOGIN_TOKEN_EXPIRE_SECONDS = 40 * 60 * 1000;
+    public static final long LOGIN_EXPIRE_SECONDS = 24 * 60 * 60 * 1000;
+    public static final long LOGIN_TOKEN_EXPIRE_SECONDS = LOGIN_EXPIRE_SECONDS;
 
     public static final String LOGIN_USER_TOKE_KEY = "LOGIN_USER_TOKE_KEY:USER_ID:";
-
-    public static final int NCPUS = Runtime.getRuntime().availableProcessors();
-
 }
