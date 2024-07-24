9c4a9106b811925b5adc986f01242d76f501fc14
wuzhenhong <wuzhenhong@banksteel.com>
2024-07-24 18:55:54 +0800

    feat：整合一体环境（nginx，mysql，项目，jdk）脚本

--- docker-build_whole.sh
+++ docker-build_whole.sh
@@ -7,19 +7,12 @@ mkdir -p /usr/logs/
 docker build -t lx-doc:1.0 .
 
 # 你可以拉取github上的代码进行构建
-# 这里的 ARGS 参数可以直接写在 application-prod.yml 配置文件里，避免把密码登参数直接写在命令行中
 # 挂在的宿主目录请先创建
-# /usr/web/html/ 挂在的是前端资源目录
-# /usr/config/lx-doc/ 挂在 lx-doc 启动的配置文件，比如 application-prod.yml 就放在这个目录下面
-# /usr/nginx/config 放置nginx相关的配置
 # /usr/logs/lx-doc 应用的日志
 # /usr/attament/lx-doc 上传附件的存放路径
 # /usr/app/mysql mysq存放数据路径
 docker run -dit --network host --privileged \
- -v /usr/web/html/:/usr/web/html/ \
- -v /usr/config/lx-doc/:/usr/config/lx-doc/ \
  -v /var/log/nginx/:/var/log/nginx/ \
- -v /usr/nginx/config:/usr/nginx/config \
  -v /usr/logs/lx-doc:/usr/logs/lx-doc \
  -v /usr/attament/lx-doc:/usr/attament/lx-doc \
  -v /usr/app/mysql:/usr/app/mysql \
@@ -32,19 +25,13 @@ docker run -dit --network host --privileged \
 # ip地址或者修改容器的hosts，修改hosts可以在Dockerfile操作：
 
 # 你可以拉取github上的代码进行构建
-# 这里的 ARGS 参数可以直接写在 application-prod.yml 配置文件里，避免把密码登参数直接写在命令行中
 # 挂在的宿主目录请先创建
 # /usr/web/html/ 挂在的是前端资源目录
-# /usr/config/lx-doc/ 挂在 lx-doc 启动的配置文件，比如 application-prod.yml 就放在这个目录下面
-# /usr/nginx/config 放置nginx相关的配置
 # /usr/logs/lx-doc 应用的日志
 # /usr/attament/lx-doc 上传附件的存放路径
 # /usr/app/mysql mysq存放数据路径
 docker run -dit -p 9222:9222 -p 8090:8090 --privileged \
- -v /usr/web/html/:/usr/web/html/ \
- -v /usr/config/lx-doc/:/usr/config/lx-doc/ \
  -v /var/log/nginx/:/var/log/nginx/ \
- -v /usr/nginx/config:/usr/nginx/config \
  -v /usr/logs/lx-doc:/usr/logs/lx-doc \
  -v /usr/attament/lx-doc:/usr/attament/lx-doc \
  -v /usr/app/mysql:/usr/app/mysql \
