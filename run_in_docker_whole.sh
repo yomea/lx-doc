40596bf15830d55b5efc03d2b3fcf21dcf1a8908
wuzhenhong <wuzhenhong@banksteel.com>
2024-07-29 18:09:27 +0800

    fix：修改脚本初始化启动，关闭脚本

--- run_in_docker_whole.sh
+++ run_in_docker_whole.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 
-# 后台模式启动 nginx，并指定nginx配置文件路径，如果使用了 docekr 部署，这个配置文件请映射到宿主机
-# 以便随时修改，每次修改之后可通过 docker exec -it 【容器id】 nginx -s reload -c /usr/nginx/config/nginx.conf
-# 重启 nginx
-nginx -g "daemon on;" -c /usr/nginx/config/nginx.conf
-
 SERVICE=$(cd $(dirname $0); pwd | awk -F '/' '{print $(NF)}')
+SERVICE_DIR="/usr/app/${SERVICE}/${SERVICE}"
 MEMORY=512m
-
-# 启动mysql
-nohup /bin/sh /usr/app/${SERVICE}/mysql_init_start.sh &
-
-MYSQL_PORT=${MYSQL_PORT:-"3306"}
-while true; do
-  if netstat -tuln | grep -q ":$MYSQL_PORT"; then
-      echo "MySQL is currently running..."
-      break
-  else
-      echo "wait mysql start..."
-      sleep 1
-  fi
-done
 
 Start() {
     mem=$1
@@ -29,21 +11,57 @@ Start() {
         mem=$MEMORY
     fi
 
-    proc=$(ps -ef | grep ${SERVICE} | grep -v grep | wc -l)
+    proc=$(ps -ef | grep "$SERVICE_DIR" | grep -v grep | wc -l)
     if [[ $proc != 0  ]];then
         exit 5
     fi
 
-    java -server -Xms${mem} -Xmx${mem}  -jar /usr/app/${SERVICE}/${SERVICE}.jar  $ARGS
+    java -server -Xms${mem} -Xmx${mem}  -jar "$SERVICE_DIR".jar  $ARGS
 }
 
+initStart() {
+    # 后台模式启动 nginx，并指定nginx配置文件路径，如果使用了 docekr 部署，这个配置文件请映射到宿主机
+    # 以便随时修改，每次修改之后可通过 docker exec -it 【容器id】 nginx -s reload -c /usr/nginx/config/nginx.conf
+    # 重启 nginx
+    nginx -g "daemon on;" -c /usr/nginx/config/nginx.conf
+
+    # 启动mysql
+    /bin/sh /usr/app/${SERVICE}/mysql_init_start.sh &
+
+    MYSQL_PORT=${MYSQL_PORT:-"3306"}
+    while true; do
+      if netstat -tuln | grep -q ":$MYSQL_PORT"; then
+          echo "MySQL is currently running..."
+          break
+      else
+          echo "wait mysql start..."
+          sleep 1
+      fi
+    done
+
+    Start $1
+}
 
 Stop() {
+    in_stop_count=0
      while true; do
-        proc=$(ps -ef | grep ${SERVICE} | grep -v grep | wc -l)
+        proc=$(ps -ef | grep "$SERVICE_DIR" | grep -v grep | wc -l)
         if [[ $proc != 0 ]]; then
-            echo "stop ${SERVICE} ..."
-            /usr/bin/ps -ef | grep ${SERVICE} | grep -v grep | awk '{print $2}'| xargs kill -5
+            if [[ $in_stop_count == 0 ]]; then
+              if ps -ef | grep "$SERVICE_DIR" | grep -v grep | awk '{print $1}'| xargs kill -5 > /dev/null; then
+                echo "Attempt to gracefully close the ${SERVICE}"
+              else
+                echo "kill command exec error..."
+                break
+              fi
+            elif [[ $in_stop_count -gt 30 ]]; then
+                echo "Close timeout, Forcefully close the ${SERVICE}"
+                ps -ef | grep "$SERVICE_DIR" | grep -v grep | awk '{print $1}'| xargs kill -9
+                break
+            else
+                echo "Closed and waited for $in_stop_count seconds"
+            fi
+            in_stop_count=$[in_stop_count+1]
             sleep 1
         else
             echo "${SERVICE} not running now..."
@@ -61,6 +79,9 @@ Restart() {
 case $1 in
     start|run)
         Start $2
+        ;;
+    initStart)
+        initStart $2
         ;;
     stop)
         Stop
