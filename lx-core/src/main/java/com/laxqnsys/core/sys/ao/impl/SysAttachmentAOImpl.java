5315225d5945436cb4d6dba0f003d8a533076a03
wuzhenhong <wuzhenhong@banksteel.com>
2024-08-06 09:32:24 +0800

    feat：优化附件上传的代码

--- lx-core/src/main/java/com/laxqnsys/core/sys/ao/impl/SysAttachmentAOImpl.java
+++ lx-core/src/main/java/com/laxqnsys/core/sys/ao/impl/SysAttachmentAOImpl.java
@@ -33,24 +33,28 @@ public class SysAttachmentAOImpl implements SysAttachmentAO {
 
     private Pattern imgBase64Pattern = Pattern.compile("^data:image/[a-zA-Z]+;base64,.*");
 
+    private final String FIX_STATIC_PATH = "/static/";
+
     @Override
     public List<String> uploadFiles(MultipartFile[] file) {
         List<String> urlList = Arrays.stream(file).map(f -> {
             String fileUploadPath = lxDocWebProperties.getFileUploadPath();
-            String randomDir = fileUploadPath + File.separator + UUID.randomUUID();
+            String uuid = UUID.randomUUID().toString();
+            String randomDir = fileUploadPath + File.separator + uuid;
             File outFile = new File(randomDir);
             if (!outFile.exists() && !outFile.mkdirs()) {
                 throw new BusinessException(ErrorCodeEnum.ERROR.getCode(),
                     String.format("附件目录%s创建失败！", randomDir));
             }
-            String path = randomDir + File.separator + f.getOriginalFilename();
+            String shortPath = uuid + File.separator + f.getOriginalFilename();
+            String path = fileUploadPath + File.separator + shortPath;
             try (InputStream inputStream = f.getInputStream();
                 FileOutputStream outputStream = new FileOutputStream(path)) {
                 IoUtil.copy(inputStream, outputStream);
             } catch (IOException e) {
                 throw new BusinessException(ErrorCodeEnum.ERROR.getCode(), "上传附件失败！", e);
             }
-            return "/static/" + randomDir;
+            return FIX_STATIC_PATH + shortPath;
         }).collect(Collectors.toList());
         return urlList;
     }
