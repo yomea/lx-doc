98066ed6527eb9385dc189673dc9e9323d2c82f0
wuzhenhong <wuzhenhong@banksteel.com>
2025-03-06 21:03:52 +0800

    feat：Implement multiple file upload methods

---
+++ lx-core/src/main/java/com/laxqnsys/core/properties/LocalFileUploadProperties.java
@@ -0,0 +1,13 @@
+package com.laxqnsys.core.properties;
+
+import lombok.Data;
+
+/**
+ * @author wuzhenhong
+ * @date 2025/3/6 20:38
+ */
+@Data
+public class LocalFileUploadProperties {
+
+    private String filePath;
+}
